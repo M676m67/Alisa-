558bdcfea1a832924b5791e74d4ff3a945cbd602
M676m67 <m67m67mn@gmail.com>
2025-06-24 12:08:42 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const list = document.querySelectorAll('.list');
+
+list.forEach(item => {
+    item.addEventListener('click', function() {
+        list.forEach(li => li.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
