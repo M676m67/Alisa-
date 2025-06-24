fe44e08c19cd8b10f2453d643695ab7598c3c3e5
M676m67 <m67m67mn@gmail.com>
2025-06-24 12:31:41 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,16 @@
-function login() {
-    const username = document.getElementById('username').value;
-    if (username.trim() !== "") {
-        localStorage.setItem("username", username);
-        window.location.href = "home.html";
-    } else {
-        alert("Please enter your name");
-    }
-}
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const storedEmail = localStorage.getItem("userEmail");
+  const storedPassword = localStorage.getItem("userPassword");
+
+  if (email === storedEmail && password === storedPassword) {
+    alert("تم تسجيل الدخول بنجاح!");
+    window.location.href = "home.html"; // الصفحة الرئيسية
+  } else {
+    alert("بيانات الدخول غير صحيحة");
+  }
+});
